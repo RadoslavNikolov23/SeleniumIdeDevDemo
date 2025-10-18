0c50c42f157969e05d6e56b02c9101b41b82c544
Rado <sitcom93@abv.bg>
2025-10-19 00:39:06 +0300

    Add new Arguments for ChromeDriver

--- SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
+++ SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
@@ -22,11 +22,10 @@ public class TC01IfUserIsInvalidTryAgainTest
     public void SetUp()
     {
         ChromeOptions options = new ChromeOptions();
-        options.AddArgument("headless");
-        options.AddArgument("no-sandbox");
-        options.AddArgument("disable-dev-shm-usage");
-        options.AddArgument("disable-gpu");
-        options.AddArgument("window-size=1920x1080");
+        options.AddArgument("--headless=new");
+        options.AddArgument("--no-sandbox");
+        options.AddArgument("--disable-dev-shm-usage");
+        options.AddArgument($"--user-data-dir=/tmp/chrome-profile-{Guid.NewGuid()}");
 
         driver = new ChromeDriver();
         js = (IJavaScriptExecutor)driver;
